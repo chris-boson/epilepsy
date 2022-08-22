9eee71ca1e26f387be14f4687e7dbcc250b6b6a7
Rob Sylvester <robmsylvester@gmail.com>
2022-08-21 22:33:49 -0700

    add additional python class info

--- code/CodingBasics/scripts/hello_world.py
+++ code/CodingBasics/scripts/hello_world.py
@@ -1,9 +1,13 @@
+# Example 1 - hello_world.py
 import random
 
 
 def main():
-    print(f"Hello World {random.random()}")
+    print("Hello World")
+    a = random.random()
+    return a
 
 
 if __name__ == "__main__":
-    main()
+    result = main()
+    print(result)
